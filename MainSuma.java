0720ac5a2386a1b875c4bfeb45f47d53ed0298b2
alfonsosolis <ponchomoran7@gmail.com>
2024-05-02 01:12:14 -0600

    version 1

---
+++ MainSuma.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MainSuma {
+    public static void main(String[] args) {
+        Scanner op = new Scanner(System.in);
+        int y, x;
+
+        System.out.println("ingresa un numero entero");
+
+        x = op.nextInt();
+
+        System.out.println("ingresa otro numero entero");
+
+        y = op.nextInt();
+
+        Suma evaluar = new Suma();
+        evaluar.x = x;
+        evaluar.y = y;
+        evaluar.mostrarResultado();
+    }
+
+}
---
+++ Suma.java
@@ -0,0 +1,9 @@
+public class Suma {
+    int x, y;
+    int evaluar;
+
+    public void mostrarResultado() {
+        System.out.print("sera: " + (this.x + this.y));
+
+    }
+}    
